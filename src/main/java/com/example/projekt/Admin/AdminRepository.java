2f0d05c204cb2a628d60f744d2427d7d68636807
karolsinko <karol.sinko30@gmail.com>
2023-01-31 12:02:54 +0100

    Bakalárska práca 1.14 (Admin 1.2)

--- src/main/java/com/example/projekt/Admin/AdminRepository.java
+++ src/main/java/com/example/projekt/Admin/AdminRepository.java
@@ -3,6 +3,9 @@ package com.example.projekt.Admin;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AdminRepository extends CrudRepository<AdminEntity, Long> {
+    Optional<AdminEntity> findByUsername(String username);
 }
--- src/main/java/com/example/projekt/Admin/AdminService.java
+++ src/main/java/com/example/projekt/Admin/AdminService.java
@@ -1,4 +1,19 @@
 package com.example.projekt.Admin;
 
+import java.util.Optional;
+
 public class AdminService {
+    private final AdminRepository adminRepository;
+
+    public AdminService(AdminRepository adminRepository) {
+        this.adminRepository = adminRepository;
+    }
+
+    public Optional<AdminEntity> login(String username, String password) {
+        Optional<AdminEntity> admin = adminRepository.findByUsername(username);
+        if (admin.isPresent() && admin.get().getPassword().equals(password)) {
+            return admin;
+        }
+        return Optional.empty();
+    }
 }
